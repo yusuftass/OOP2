be5d416e6de371fdd313374a6d427c2b12082fa9
yusuf taş <yusuf.yusuf21@hotmail.com>
2021-01-24 00:14:12 +0300

    Add project files.

---
+++ OOP3/BasvuruManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    class BasvuruManager
+    {
+        //Method injection
+        public void BasvuruYap(IKrediManager krediManager, ILoggerService loggerService)
+        {
+            //Başvuran bilgilerini değerlendirme
+            //
+            
+            krediManager.Hesapla();
+            loggerService.Log();
+        }
+
+        public void KrediOnBilgilendirmesiYap(List<IKrediManager> krediler)
+        {
+            foreach (var kredi in krediler)
+            {
+                kredi.Hesapla();
+                //interfacleri birbirlerinin alternatifi olan ama kod içerikleri farklı olan durumlar için kullanırız
+
+            }
+        }
+    }
+}
---
+++ Oop2/Musteri.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oop2
+{
+    class Musteri
+    {
+
+        public int Id { get; set; }
+        public string MusteriNo { get; set; }
+
+        // bir nesnede bir değeri kullanmak zorunda değilsen o yerde hata yapıyorsun demektir.
+
+    }
+}
